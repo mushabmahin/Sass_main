f34de4da0c415a2b48051d68d820386cc34481e3
Adarsh-M-Nair <adarshvaikhary@gmail.com>
2025-08-06 07:03:04 +0530

    Fifth Commit

---
+++ components/Solution-Components/sap/technology.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="technology-page">
+    <div class="container mx-auto px-4 py-8">
+      <div class="flex justify-center">
+        <img 
+          src="/images/SAP Product Logos Collection.png" 
+          alt="SAP Product Logo Collection" 
+          class="max-w-full h-auto"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Component logic can be added here if needed
+</script>
+
+<style scoped>
+.technology-page {
+  min-height: 100vh;
+  background-color: #ffffff;
+  color: white;
+}
+
+.container {
+  max-width: 1200px;
+}
+</style>
